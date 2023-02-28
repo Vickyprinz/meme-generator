df6fc2cd7778fc5dacab541b40287bef813d3c12
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 19:05:01 +0300

    added app components

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,11 @@ import Memes from './pages/Memes';
 import API from './API';
 import Dashboard from './pages/Dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: '', 
+    myMemes: []
+  }
 }
 
 export default App;
