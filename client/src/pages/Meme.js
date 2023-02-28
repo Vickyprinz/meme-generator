bf416f0bd410b49836b8498ea73e81326a89f7ef
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 12:54:54 +0300

    added meme class with some components

--- client/src/pages/Meme.js
+++ client/src/pages/Meme.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 import MemesCollection from '../components/memes-collection';
 import MemeForm from '../components/Meme-form'
+
+
+class Memes extends Component {
+    state = { 
+        memes: [],
+        selectedMeme: null,
+        modalIsOpen: false,
+        currentImgBase64: null
+     }
+    }
