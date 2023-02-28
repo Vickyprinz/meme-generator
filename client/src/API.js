682fc98d976a4e4cfda08c42bb422816eecb4e8e
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 18:58:15 +0300

    Define API class with static methods and URLs

---
+++ client/src/API.js
@@ -0,0 +1,17 @@
+class API {
+    static baseUrl = 'http://localhost:3001' 
+    static signinUrl = API.baseUrl + '/signin'
+    static signupUrl = API.baseUrl + '/signup'
+    static validateUrl = API.baseUrl + '/validate'
+    static dashboardUrl = API.baseUrl + '/dashboard'
+    static myMemes = API.baseUrl + '/customised_memes'
+ 
+    static signin (user) {
+     return fetch(this.signinUrl, {
+         method: "POST",
+         headers: {'Content-Type': 'application/json'},
+         body: JSON.stringify(user)
+     }).then(resp => resp.json())
+ 
+    }
+}
