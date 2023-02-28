72ee570fe243e4068edfb0db7c8a40cf1678bbfd
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 12:35:59 +0300

    Add Components

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,3 +3,9 @@ import React, { Component } from 'react';
 
 import CustomisedMeme from '../components/CustomisedMeme'
 import NavBar from '../components/NavBar'
+import API from '../API'
+
+
+class Dashboard extends Component {
+    
+}
