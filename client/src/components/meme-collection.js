96f19749d2d39c712192481af77cf10a611a354d
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 12:23:07 +0300

    Pass memes array as props to MemesCollection component feat: Add Meme component to MemesCollection Pass meme object and handleClick function as props to Meme component from MemesCollection

--- client/src/components/meme-collection.js
+++ client/src/components/meme-collection.js
@@ -3,5 +3,13 @@ import React, {Component} from 'react';
 import Meme from './Meme'
 
 class MemesCollection extends Component {
-    
+    render() { 
+        const {memes} = this.props
+        const meme = memes.map(meme => (
+          <Meme 
+            key={meme.id} 
+            meme={meme}
+            handleClick={this.props.handleClick}
+          />))
+}
 }
