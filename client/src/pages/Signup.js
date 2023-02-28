798a953ea79dc2a6fd42db93321240bf7cfd0264
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 18:52:56 +0300

    Add form input fields to Signup components

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -23,4 +23,34 @@ class Signup extends Component {
                 }
             })
     }
+    handleChange = e => {
+        e.preventDefault()
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
+    render() {
+        return (  
+            // <!-- Default form login -->
+            <div>
+                  <form className="text-center border border-light p-5">
+    
+                    <p className="h4 mb-4">Sign up</p>
+    
+                    <input 
+                        name='username' 
+                        id="defaultSignup" 
+                        onChange={this.handleChange} 
+                        value={this.state.username} 
+                        className="form-control mb-4" 
+                        placeholder="username" 
+                    />
+                    <input 
+                        name='password' 
+                        type='password'
+                        id="defaultSignupPassword" 
+                        onChange={this.handleChange} 
+                        value={this.state.password} 
+                        className="form-control mb-4" 
+                        placeholder="Password" 
+                    />
 }
