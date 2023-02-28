7bed33db6bcaeb549c072f1a58fd9438658ef47a
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 12:53:28 +0300

    imorted required files

---
+++ client/src/pages/Meme.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import NavBar from '../components/NavBar';
+import MemesCollection from '../components/memes-collection';
+import MemeForm from '../components/Meme-form'
