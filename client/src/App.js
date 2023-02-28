c8edd31276f0d870d96cf92033c1710de5e98a3f
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 19:09:59 +0300

    added router function

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+
+
 import React, {Component} from 'react';
 import './App.css';
 import { Route, Switch, withRouter } from 'react-router-dom'
@@ -9,17 +11,20 @@ import Memes from './pages/Memes';
 import API from './API';
 import Dashboard from './pages/Dashboard'
 
+
 class App extends Component {
   state = {
     username: '', 
     myMemes: []
   }
-}
-signin = (username, token) => {
-  localStorage.setItem('token', token)
-  this.setState({username}, () => {
-    this.props.history.push('/memes')
-  })
+
+  signin = (username, token) => {
+    localStorage.setItem('token', token)
+    this.setState({username}, () => {
+      this.props.history.push('/memes')
+    })
+  }
+
   signup = (username) => {
     this.setState({username})
   }
@@ -29,6 +34,55 @@ signin = (username, token) => {
     localStorage.removeItem('token')
     this.props.history.push('/')
   }
+
+  addToMyMemes = (newMeme) => {
+    this.setState({myMemes: [...this.state.myMemes, newMeme]})
+  }
+
+  addManyToMyMemes = (newMemes) => {
+    this.setState({myMemes: [...this.state.myMemes, ...newMemes]})
+  }
+   
+  componentDidMount() {
+    API.validate()
+      .then(data => {
+        if (data.error){
+          this.props.history.push('/')
+        }
+        else {
+          this.signin(data.username, localStorage.getItem('token'))
+          API.getDashboard()
+          .then(data => {
+              if(data.error) {
+                  alert("You don't have any memes!")
+              } else {
+                  this.addManyToMyMemes(data)
+              }
+        })
+        
+      }
+    })
+  }
+
+  render() {
+    const {username, myMemes} = this.state
+    const {signin, signup, signout, addManyToMyMemes} = this
+    return (
+      <div className="App">
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/signin' component={props => <Signin {...props} signin={signin}/>} />
+            <Route exact path='/signup' component={props => <Signup {...props} signup={signup}/>} />
+            <Route exact path='/dashboard' component={props => <Dashboard {...props} username={username} signout={signout} myMemes={myMemes} addManyToMyMemes={addManyToMyMemes}/>}/>
+            <Route exact path='/memes'  component={props => <Memes {...props} username={username} signout={signout} addToMyMemes={this.addToMyMemes}/>}/>
+            <Route component={() => <h1>Page not found.</h1>} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+export default withRouter(App);
