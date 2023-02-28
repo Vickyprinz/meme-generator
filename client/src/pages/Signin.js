7915c47f542b72837516a22cbf41bdc7feac4ca1
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 18:45:49 +0300

    Add handleChange function to Signin component

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -9,4 +9,18 @@ class Signin extends Component {
         username: '',
         password: ''
      }
+     handleSubmit = (e) => {
+        // API.signin(this.state)
+        e.preventDefault()
+        API.signin(this.state)
+            .then(data => {
+                if (data.error){
+                    alert("not working!")
+                }
+                else {
+                    this.props.signin(this.state.username, data.token)
+                }
+            })
+    }
+
     }
