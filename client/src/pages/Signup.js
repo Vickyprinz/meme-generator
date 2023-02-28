a47bb62b61b90ca15bb8f8dbe1eaa762a99adf96
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-28 18:49:43 +0300

    Add handleChange function to autheticate user

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,3 +8,19 @@ class Signup extends Component {
         username: '',
         password: ''
     }
+    handleSubmit = (e) => {
+        // API.signin(this.state)
+        e.preventDefault()
+        API.signup(this.state)
+            .then(data => {
+                if (data.error){
+                    alert('not working!')
+                }
+                else {
+                    // user is authentificated!
+                    this.props.signup(this.state.username)
+                    this.props.history.push('/signin')
+                }
+            })
+    }
+}
